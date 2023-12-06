7e2ff27c931168b6f25fe9391afc97438958a5cb
Chandra Shekar Suryadevara <128857515+VOID-HACKER@users.noreply.github.com>
2023-12-06 14:52:12 -0700

    cmopeted almost receiver

--- recerivertest/Receiver.cc
+++ recerivertest/Receiver.cc
@@ -1,7 +1,8 @@
-#include "../headers/Receiver.h"
+#include "Receiver.h"
 #include <iostream>
 #include <math.h>
 #include <vector>
+#include <thread>
 #include <unistd.h>
 #include <netdb.h>
 #include <cstring>
@@ -9,7 +10,90 @@
 #include <string>
 #include <queue>
 #include <fstream>
+#include <fcntl.h> 
+#include <unistd.h>
 
+
+
+
+unsigned int Receiver::calcrc1() {
+    unsigned int crc1;
+
+    crc1 = crc32(0L, NULL, 0);
+    crc1 = crc32(crc1, (reinterpret_cast<const Bytef*>(buffer)), 8);
+    return crc1;
+}
+
+
+
+
+std::vector<uint8_t> Receiver::extract_payload(uint8_t buf[]) {
+    std::vector<uint8_t> sub_payload(datasize);
+    for (int i = 0; i < datasize; i++) {
+        sub_payload[i] = buf[12 + i];
+    }
+    return sub_payload;
+}
+
+
+unsigned int Receiver::calcrc2() {
+    uint8_t sub_payload[datasize];
+    for (int i = 0; i < datasize; i++) {
+        sub_payload[i] = buffer[12 + i];
+    }
+    unsigned long crc2;
+
+    crc2 = crc32(0L, NULL, 0);
+    crc2 = crc32(crc2, (reinterpret_cast<const Bytef*>(sub_payload)), datasize);
+    return crc2;
+}
+
+unsigned int Receiver::extractCRC(uint8_t buf[]) {
+    unsigned int crc = 0;
+    crc |= buf[CRC] << 24;
+    crc |= buf[CRC + 1] << 16;
+    crc |= buf[CRC + 2] << 8;
+    crc |= buf[CRC + 3];
+    return crc;
+}
+
+void Receiver::set_crc1(unsigned int CRC1) {
+
+    crc1 = CRC1;
+
+}
+
+unsigned int Receiver::extractCRC2(uint8_t buf[]) {
+    int CRC2 = 12 + datasize;
+    unsigned int crc = 0;
+    crc |= buf[CRC2] << 24;
+    crc |= buf[CRC2 + 1] << 16;
+    crc |= buf[CRC2 + 2] << 8;
+    crc |= buf[CRC2 + 3];
+    return crc;
+}
+
+unsigned int Receiver::extractLength(uint8_t buf[]) {
+    unsigned int length = (buf[L] << 8) | buf[L + 1];
+    return length;
+}
+
+int Receiver::getdatasize() {
+    return datasize;
+}
+
+int Receiver::getWinSize() {
+    return Winsize;
+}
+
+void Receiver::set_winsize(int win) {
+    Winsize = win;
+}
+std::queue<std::vector<uint8_t>> Receiver::get_data() {
+
+
+    return data;
+}
 
 
 int main(int argc, char* argv[]) {
@@ -39,43 +123,144 @@ int main(int argc, char* argv[]) {
         
     }
 
-
-
-     std::ofstream of;
-
-  // open file in binary mode
-  of.open(data_file.c_str(), std::ios::binary | std::ios::out);
-    if (of.fail()) {
-        std::cout << "Cannot create file" << std::endl;
-        return 1;
-    }
-
-     Receiver Receiver(argv[4], argv[5], argv[3]);
-     Receiver.receive_data();
-     if (Receiver.inspect_packet()){
-     std::queue<std::vector<uint8_t>> data = Receiver.get_data();
-    while (!data.empty()) {
-        const auto& byteVector = data.front();
-        of.write(reinterpret_cast<const char*>(byteVector.data()), byteVector.size());
-        data.pop();
-    }
-
-    if (of.fail()) {
-        std::cout << "Write failed" << std::endl;
-        of.close();
-        return 2;
-    }else{
-        std::cout<<"writing done";
-    }
-     }else{
-
-        std::cerr<<"something is wrong with the packet";
-     }
-    of.close();
+     Receiver receiver(argv[4], argv[5], argv[3]);
+     std::thread receiving_thread(&Receiver::receive_data, &receiver);
+     std::thread saving_thread(&Receiver::save_data_to_file, &receiver, data_file);
+     
+     receiving_thread.join();
+     saving_thread.join();
     return 0;
 }
+void Receiver::save_data_to_file(const std::string& data_file) {
+    // Open the file for writing
+    std::ofstream of(data_file.c_str(), std::ios::binary | std::ios::out);
+    if (of.fail()) {
+        std::cout << "Cannot create file" << std::endl;
+        return;
+    }
 
-Receiver::Receiver(const char* destinationHost, const char* destinationPort, const char* IPTYPE) {
+    while (!done_receiving) {
+        
+            std::unique_lock<std::mutex> lock(mtx); // Correct placement
+
+            // Wait until data is available for saving
+            cv.wait(lock, [this] { return !data.empty() || done_receiving; });
+
+            // Save data to file if available
+            while (!data.empty()) {
+                const auto& byteVector = data.front();
+                of.write(reinterpret_cast<const char*>(byteVector.data()), byteVector.size());
+                data.pop();
+            }
+        
+
+        // Check if done receiving
+        if (done_receiving) {
+            break;
+        }
+    }
+    if (done_receiving) {
+        // Close the file outside the loop
+        of.close();
+
+        if (of.fail()) {
+            std::cout << "Write failed" << std::endl;
+        }
+        else {
+            std::cout << "Writing done" << std::endl;
+        }
+    }
+}
+
+
+
+
+void Receiver::Send_packet(uint8_t buf[]) {
+
+    if ((buf[TP] >> 6 & 0b11) == 1) {
+        if ((buf[TP] & 0x20) == 1) {
+
+            if (sendto(sock, reinterpret_cast<const char*>(buf), headersize + datasize, 0, ptr->ai_addr, ptr->ai_addrlen) == -1) {
+                std::cerr << "Error sending stripped paylaod  packet" << std::endl;
+            }
+            else {
+
+                std::cout << "sent a stripped of packet for seq NUm " << static_cast<int>(buf[TP + 1]) << std::endl;
+            }
+        }
+    }
+}
+
+void Receiver::Send_ack_packet(const std::array<uint8_t, maxSize>& buf) {
+    if ((buf[TP] >> 6 & 0b11) == 2) {
+        if (sendto(sock, reinterpret_cast<const char*>(buf.data()), maxSize, 0, ptr->ai_addr, ptr->ai_addrlen) == -1) {
+            std::cerr << "Error sending negative ack packet" << std::endl;
+        }
+        else {
+            std::cout << "sent an Ack packet for seq Num " << static_cast<int>(buf[TP + 1]) << std::endl;
+        }
+    }
+}
+
+
+bool Receiver::is_done() {
+
+    return done_receiving;
+
+}
+
+
+
+
+void Receiver::Send_neg_ack_packet(const std::array<uint8_t, maxSize>& buf) {
+    if ((buf[TP] >> 6 & 0b11) == 3) {
+        if (sendto(sock, reinterpret_cast<const char*>(buf.data()), maxSize, 0, ptr->ai_addr, ptr->ai_addrlen) == -1) {
+            std::cerr << "Error sending negative ack packet" << std::endl;
+        }
+        else {
+            std::cout << "sent a negative Ack packet for seq Num " << static_cast<int>(buf[TP + 1]) << std::endl;
+        }
+    }
+}
+
+
+std::array<std::uint8_t, maxSize> Receiver::make_packet(unsigned int type, unsigned int TR, unsigned int Seqnum)
+{
+    uint8_t buf[maxSize];
+    for (int i = 0; i < maxSize; i++) {
+        buf[i] = buf[i] & 0x00;
+    }
+    if (type > 0 && type <= 3) {
+        // Clear the existing value and set the lowest 2 bits of type
+        buf[TP] &= 0x3f;
+        buf[TP] |= (type << 6);
+
+    }
+    if (TR == 0 || TR == 1) {
+
+        buf[TP] &= 0xDF;
+        buf[TP] |= (TR << 5) >> 2;
+
+    }
+
+
+        buf[TP + 1] &= 0;
+        buf[TP + 1] |= Seqnum;
+
+        for (int i = 0; i < datasize; i++) {
+            buf[Pay + i] &= 0x00;
+
+        }
+        std::array<uint8_t, maxSize> buf1;
+        for (int i = 0; i < maxSize; i++) {
+            buf1[i] = buf[i] & 0x00;
+        }
+
+        return buf1;
+      
+    }
+
+Receiver::Receiver(const char* destinationHost, const char* destinationPort, const char* IPTYPE){
     int ai_family = AF_UNSPEC; // Default to both IPv4 and IPv6
     if (strcmp(IPTYPE, "-4") == 0) {
         ai_family = AF_INET; // IPv4
@@ -86,157 +271,90 @@ Receiver::Receiver(const char* destinationHost, const char* destinationPort, con
         std::abort();
     }
 
-    struct addrinfo hints, *results, *ptr;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = ai_family;
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_flags = AI_PASSIVE;
-
-    int rval = getaddrinfo(NULL, destinationPort, &hints, &results); // Use NULL for AI_PASSIVE
-
-    if (rval != 0) {
+    done_receiving = false;
+        
+    if ((rval = getaddrinfo(destinationHost, destinationPort, &hints, &results)) != 0) {
         std::cerr << "Error getting address info: " << gai_strerror(rval) << std::endl;
-        std::abort();
+        std:abort();
     }
 
-    for (ptr = results; ptr != nullptr; ptr = ptr->ai_next) {
-        if ((sock = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol)) == -1) {
-            continue;
-        }
-
-        if (bind(sock, ptr->ai_addr, ptr->ai_addrlen) == -1) {
+    for (ptr = results; ptr != NULL; ptr = ptr->ai_next) {
+        if ((sock = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol)) != -1) {
+            if (bind(sock, ptr->ai_addr, ptr->ai_addrlen) != -1) {
+                break;
+            }
             close(sock);
-            continue;
         }
-        break;
     }
-
-    freeaddrinfo(results); // Free the address info
-
-    if (ptr == nullptr) {
+     if (ptr == NULL) {
         std::cerr << "Failed to bind the socket" << std::endl;
         std::abort();
     }
 }
 
 
-int Receiver::getSeqSize() {
-    return SeqnumSize;
-}
 
-std::queue<std::vector<uint8_t>> Receiver::get_data(){
-
-
-    return data;
-}
-void Receiver::receive_data() {
-    sockaddr localAddr;
-    socklen_t localAddrLen = sizeof(localAddr);
-    getsockname(sock, &localAddr, &localAddrLen);
-
-    if (localAddr.sa_family == AF_INET) {
-        sockaddr_in* ipv4LocalAddr = (sockaddr_in*)&localAddr;
-        std::cout << "Bound to port: " << ntohs(ipv4LocalAddr->sin_port) << std::endl;
-    } else if (localAddr.sa_family == AF_INET6) {
-        sockaddr_in6* ipv6LocalAddr = (sockaddr_in6*)&localAddr;
-        std::cout << "Bound to port: " << ntohs(ipv6LocalAddr->sin6_port) << std::endl;
-    }
-
-    std::cout << "Waiting for packets..." << std::endl;
-
+void Receiver::receive_data(){
     fd_set read_fds;
     FD_ZERO(&read_fds);
     FD_SET(sock, &read_fds);
 
     struct timeval timeout;
-    timeout.tv_sec = 15; // 15-second timeout
+    timeout.tv_sec = 10; // 15-second timeout
     timeout.tv_usec = 0;
+    while (!done_receiving) {
+        sockaddr localAddr;
+        sockaddr_storage clientAddr;
+        socklen_t localAddrLen = sizeof(localAddr);
+        socklen_t clientAddrLen = sizeof(clientAddr);
 
-    int activity = select(sock + 1, &read_fds, nullptr, nullptr, &timeout);
+        getsockname(sock, &localAddr, &localAddrLen);
+        ssize_t receivedBytes = 0;
+        if (localAddr.sa_family == AF_INET) {
+            sockaddr_in* ipv4LocalAddr = (sockaddr_in*)&localAddr;
+            std::cout << "Bound to port: " << ntohs(ipv4LocalAddr->sin_port) << std::endl;
 
-    if (activity == -1) {
-        std::cerr << "Error in select()" << std::endl;
-        return;
-    } else if (activity == 0) {
-        std::cerr << "No packets received within 15 seconds." << std::endl;
-        return;
+            receivedBytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+        }
+        else if (localAddr.sa_family == AF_INET6) {
+            sockaddr_in6* ipv6LocalAddr = (sockaddr_in6*)&localAddr;
+            std::cout << "Bound to port: " << ntohs(ipv6LocalAddr->sin6_port) << std::endl;
+            receivedBytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+        }
+     
+        if (receivedBytes == -1) {
+            std::cerr << "Error receiving data: " << strerror(errno) << std::endl;
+        }
+        else if (receivedBytes == 0) {
+            std::cout << "No data received" << std::endl;
+        }
+        else {
+            inspect_packet();
+            std::cout << "Packet received. Resetting timer." << std::endl;
+            done_receiving = true;
+        }
+        
+        int activity = select(sock + 1, &read_fds, nullptr, nullptr, &timeout);
+
+        if (activity == -1) {
+            std::cerr << "Error in select()" << std::endl;
+            std::abort();
+        }
+        else if (activity == 0) {
+            std::cout << "Timeout occurred. No data received within 60 seconds." << std::endl;
+            done_receiving = true;
+            break;
+        }
+        
     }
 
-    sockaddr_storage clientAddr;
-    socklen_t clientAddrLen = sizeof(clientAddr);
-    ssize_t receivedBytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
 
-    if (receivedBytes > 0) {
-        std::cout << "Received: " << std::endl;
-        // Process received data here if needed
-    } else {
-        std::cerr << "Error receiving data" << std::endl;
     }
-}
-
-
-
-
-
-unsigned int Receiver::calcrc1(){
-     unsigned int crc1;
-            
-    crc1 = crc32(0L, NULL, 0);
-    crc1 = crc32(crc1, (reinterpret_cast<const Bytef*>(buffer)),8 );
-    return crc1;
-}
-
-
-
-
-std::vector<uint8_t> Receiver::extract_payload(uint8_t buf[]){
-    std::vector<uint8_t> sub_payload(datasize); 
-        for (int i = 0; i < datasize; i++) {
-                sub_payload[i] = buf[12 + i];
-            }
-    return sub_payload;
-}
-
-
-unsigned int Receiver::calcrc2(){
-      uint8_t sub_payload[datasize];
-    for (int i = 0; i < datasize; i++) {
-                sub_payload[i] = buffer[12 + i];
-            }
-            unsigned long crc2;
-
-            crc2 = crc32(0L, NULL, 0);
-            crc2 = crc32(crc2, (reinterpret_cast<const Bytef*>(sub_payload)), datasize);
-    return crc2;
-}
-
-unsigned int Receiver::extractCRC(uint8_t buf[]) {
-    unsigned int crc = 0;
-    crc |= buf[CRC] << 24;
-    crc |= buf[CRC + 1] << 16;
-    crc |= buf[CRC + 2] << 8;
-    crc |= buf[CRC + 3];
-    return crc;
-}
-
-unsigned int Receiver::extractCRC2(uint8_t buf[]) {
-    int CRC2 = 12 + datasize;
-    unsigned int crc = 0;
-    crc |= buf[CRC2] << 24;
-    crc |= buf[CRC2 + 1] << 16;
-    crc |= buf[CRC2 + 2] << 8;
-    crc |= buf[CRC2 + 3];
-    return crc;
-}
-
-unsigned int Receiver::extractLength(uint8_t buf[]) {
-    unsigned int length = (buf[L] << 8) | buf[L + 1];
-    return length;
-}
-
-int Receiver::getdatasize() {
-    return datasize;   
-}
+        
 
 
 
@@ -244,83 +362,97 @@ int Receiver::getdatasize() {
 bool Receiver::inspect_packet(){
     uint8_t y = nextseq;
     if((buffer[TP] >> 6) == 0x1){
-        if((buffer[TP] & 0x20) == 0){
-            if(buffer[TP+1]  == y){
+        if((buffer[TP] & 0x20) == 0x0){
+            if(acknowledge_check(buffer[TP+1]) && data.empty()){
                 unsigned int local_crc1 = calcrc1();
-                
                 if(local_crc1 == extractCRC(buffer)){
                     datasize = extractLength(buffer);
                     unsigned int local_crc2 = calcrc2();
                     unsigned int given_crc2 = extractCRC2(buffer);
                     if(local_crc2 == given_crc2){
+                        if (sizeof(data) == 0) {
+                            set_winsize(buffer[TP] & 0x2B67);
+                            set_crc1(local_crc1);
+                        }
                          data.push(extract_payload(buffer));
+                         nextseq++;
+                         recent_ack = buffer[TP + 1];
+                         Send_ack_packet(make_packet(2, 0, nextseq - 1));
+                         cv.notify_one();
+                         std::cout << "Sent a acknoledgement for " << nextseq - 1 << " packet"<<std::endl;
                         return true ;
                     }else {
                         std::cerr<<"Not matching CRC's computed CRC2:"<<local_crc2<<"found CRC1:"<<given_crc2;
                         return false;
-                }}else {
+                    }
+                }else {
                     std::cerr<<"Not matching CRC's computed CRC1:"<<local_crc1<<"found CRC1:"<<extractCRC(buffer);
                     return false;
                 }
-            } else{
+            }
+            else if (acknowledge_check(buffer[TP + 1])) {
+                unsigned int local_crc1 = calcrc1();
+                if ( local_crc1 == extractCRC(buffer)) {
+                    datasize = extractLength(buffer);
+                    unsigned int local_crc2 = calcrc2();
+                    unsigned int given_crc2 = extractCRC2(buffer);
+                    if (local_crc2 == given_crc2) {
+                        data.push(extract_payload(buffer));
+                        recent_ack = buffer[TP + 1];
+                        Send_ack_packet(make_packet(2, 0, nextseq - 1));
+                        std::cout << "Sent a acknoledgement for " << nextseq - 1 << "packet" << std::endl;
+                        cv.notify_one();
+                        return true;
+                    }
+                    else {
+                        std::cerr << "Not matching CRC's computed CRC2:" << local_crc2 << "found CRC1:" << given_crc2;
+                        return false;
+                    }
+                }
+                else {
+                    std::cerr << "Not matching CRC's computed CRC1 which was received with first packet:" << local_crc1 << "found CRC1:" << extractCRC(buffer);
+                    return false;
+                }
+            
+            }else {
                 std::cerr << "Seq Num Not Right";
                 return false;
             }    
-        } else{
-            std::cerr << "TR is not right";
+        }
+        else if ((buffer[TP] & 0x20) == 0x1) { // send a negative acknowledgment
+            Send_neg_ack_packet(make_packet(3, 0, nextseq));
+            std::cout << "Sent a begative acknoledgement for " << nextseq << "packet" << std::endl;
+            return false;
+
+        }
+        else {
+            std::cerr << "TR is wrong right";
             return false;
         }
-    }else{
+    }
+    else if ((buffer[TP] >> 6) == 0x2) {  //ack pacekt
+
+        std::cout << "Receieved a acknowledge pacekt"<<std::endl;
+        return false;
+
+    }
+    else if ((buffer[TP] >> 6) == 0x3) {  //negative ack pacekt
+
+        std::cout << "Receieved a negative acknowledge pacekt" << std::endl;
+        return false;
+
+    }else { // if type is other
         std::cerr << "Type is differnt cannot get data";
         return false;
     }
 }
 
 
+bool Receiver::acknowledge_check(int n){// do from here see the logic and recent ack declaration
 
-/*
-
-
-bool Receiver::acknowledge_check(int n){
-
-if(n == recent_ack + 1 || getSeqSize() - (recent_ack + 1 ) == 0 ){
+if(n == recent_ack + 1 || getWinSize() - (recent_ack + 1 ) == 0 ){
     return true;
 } else {
     return false;
 }
 }
-
-void Receiver::data_received() {
-    if (acknowledge_check(seq)) {
-        set_recent_ack(seq);
-
-        // Check if the buffer is not empty
-        if (!buffer.empty() && acknowledge_check(buffer.front())) {
-            int front = buffer.front();
-            buffer.erase(buffer.begin());
-            data_received(front);
-        }
-
-    }
-    else {
-        buffer.push_back(seq);
-    }
-}
-
-
-std::vector<int> Receiver::get_ack() {
-    std::vector<int> ack = buffer;
-    ack.insert(ack.end(), recent_ack);
-    return ack;
-}
-
-
-int Receiver::get_recent_ack() {
-    return recent_ack;
-}
-
-void Receiver::set_recent_ack(int ack) {
-    recent_ack = ack;
-}
-
-*/
--- recerivertest/Receiver.h
+++ recerivertest/Receiver.h
@@ -6,6 +6,8 @@
 #include <string>
 #include <queue>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 #include <netdb.h>
 
 const int maxSize = 528;
@@ -14,15 +16,15 @@ class Receiver{
 
 public:
 Receiver(const char* destinationHost, const char* destinationPort, const char* IPTYPE);
-~Receiver();
 
 /*Checks if the acknowledge one is the next seq number or last seq number */
 bool acknowledge_check (int n);
 void receive_data();
 /*Checks if teh seq number passed is the consecutive number if true checks 
 is buffer is empty or the buffer has previous  seq numbers or it adds the seq number to buffer*/
-void data_received(int seq);
-int getSeqSize();
+
+int getWinSize();
+void save_data_to_file(const std::string& data_file);
 /*concats buffer with recent_ack*/
 int get_recent_ack();
 bool save_packet(std::string file_name);
@@ -30,16 +32,22 @@ bool inspect_packet();
 unsigned int calcrc1();
 unsigned int extractCRC(uint8_t buf[]);
 unsigned int extractCRC2(uint8_t buf[]);
-
+bool is_done();
+void Send_packet(uint8_t buf[]);
+void Send_neg_ack_packet(const std::array<uint8_t, maxSize>& buf);
+void Send_ack_packet(const std::array<uint8_t, maxSize>& buf);
+std::array<std::uint8_t, maxSize> make_packet(unsigned int Type, unsigned int TR, unsigned int Seqnum);
 unsigned int calcrc2();
 /*setter function for recent ack*/
-void set_recent_ack(int ack);
-std::vector<int> get_ack();
+void set_winsize(int win);
+void set_crc1(unsigned int CRC1);
 unsigned int extractLength(uint8_t buf[]);
-
+std::queue<std::vector<uint8_t>> get_data();
 std::vector<uint8_t> extract_payload(uint8_t buf[]);
 int getdatasize();
 private:
+std::mutex mtx;  // Mutex for synchronization
+std::condition_variable cv;  // Condition variable for signaling
 const int L = 2;
 const int T = 4;
 const int TP = 0;
@@ -50,10 +58,12 @@ int sock, rval;
 int datasize;
 int nextseq = 0;
 struct addrinfo hints, * results, * ptr;
-int SeqnumSize;
+int Winsize;
+unsigned int crc1;
+bool done_receiving;
 uint8_t buffer[maxSize];
-std::queue<std::vector<uint8_t>> data;
-int recent_ack;
+std::queue<std::vector<uint8_t>> data; 
+int recent_ack = -1;
 };
 
 #endif
